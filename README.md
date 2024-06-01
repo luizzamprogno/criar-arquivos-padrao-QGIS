8f9b697b710f65b46f824ed25ca35012bed5082e
luizzamprogno <lpzampronio@gmail.com>
2024-06-01 08:26:51 -0300

    'Primeiro commit'

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Automação para criar arquivos shapefile no QGIS com PYTHON
 
-## Nosso objetivo aqui é automatizar o processo de criação de várias camadas shapefile padrões no QGIS utiuzando PYTHON, passando como argumentos, o nome da camada, o tipo de geometria e os campos, com nome e tipo (Inteiro, Decimal, texto) que queremos criar
+# Nosso objetivo aqui é automatizar o processo de criação de várias camadas shapefile padrões no QGIS utiuzando PYTHON, passando como argumentos, o nome da camada, o tipo de geometria e os campos, com nome e tipo (Inteiro, Decimal, texto) que queremos criar.
 
+### Observação: não criamos nenhuma feature(geometrias), somente as camadas com seus campos na tabela de atributos, ou seja, entende-se que
+### as features serão feitas posteriormente à criação dessas camadas
 
---
+++ app.py
@@ -0,0 +1,51 @@
+
+import os
+
+# Criar o diretorio pai
+diretorio = 'C:/Meus documentos/Projetos Python/Criar shapefiles/criar-arquivos-padrao-QGIS/arquivos/'
+
+# Declarar variáveis - sistema de coordenadas, nomes e tipos de geometrias dos shapefiles
+epsg = 'EPSG:31982'
+
+shapefiles = {
+    'Talhao':'Polygon',
+    'App':'Polygon',
+    'Obstaculos':'Polygon',
+    'Local':'Point',
+    'Postes':'Point',
+    'Estradas':'LineString',
+    'Basculamento':'LineString',
+    'Arvores':'Polygon',
+    'Entrada':'Point',
+    'Saida':'Point',
+    'Sede':'Point',
+    'Canal':'Polygon',
+    'Canal escoadouro':'Polygon',
+    'Mato':'Polygon',
+    'ln_poste':'LineString',
+    'Ventosa':'Point',
+    'Caixa de agua':'Polygon',
+    'Sem cana':'Polygon',
+    'Pedreira':'Polygon',
+    'pt_Arvores':'Point'
+}
+
+
+for key, value in shapefiles.items():
+
+    camada = QgsVectorLayer(value + f'?crs={epsg}', key, 'memory')
+
+    camada.dataProvider().addAttributes([QgsField('ID', QVariant.Int)])
+    camada.updateFields()
+
+    url_final = os.path.join(diretorio, key)
+
+    if os.path.isfile(url_final + '.shp'):
+        print(f'O Arquivo {url_final} já existe, altere o diretório no início do código')
+        print('')
+
+    else:
+        QgsVectorFileWriter.writeAsVectorFormat(camada, url_final, 'utf-8', camada.crs(), 'ESRI Shapefile')
+        print(f'Arquivo {key} criado com sucesso no diretório {url_final}')
+        print('')
+        iface.addVectorLayer(str(url_final) + '.shp', '', 'ogr')
